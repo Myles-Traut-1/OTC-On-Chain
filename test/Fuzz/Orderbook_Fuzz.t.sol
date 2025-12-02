df655dc917ea2808a0a33a50b9d062755bc385c8
Myles-Traut <myles@linumlabs.com>
2025-12-02 13:37:55 +0200

    Orderbook: Fuzz CreateTokenOrder Test

--- test/Fuzz/Orderbook_Fuzz.t.sol
+++ test/Fuzz/Orderbook_Fuzz.t.sol
@@ -12,7 +12,7 @@ contract OrderbookFuzzTest is TestSetup {
         uint256 _offeredAmount,
         uint64 _validFrom,
         uint64 _validUntil,
-        uint128 _maxSlippageBps
+        uint128 _slippageBps
     ) public {
         uint256 minOfferAmount = orderbook.MIN_OFFER_AMOUNT();
         uint256 maxSlippageBps = orderbook.MAX_SLIPPAGE();
@@ -36,8 +36,47 @@ contract OrderbookFuzzTest is TestSetup {
             bound(_validUntil, _validFrom + 1, _validFrom + 30 days)
         );
 
-        _maxSlippageBps = uint128(
-            bound(_maxSlippageBps, minSlippageBps, maxSlippageBps)
+        _slippageBps = uint128(
+            bound(_slippageBps, minSlippageBps, maxSlippageBps)
         ); // 0.5% - 2%
+
+        vm.startPrank(maker);
+        Orderbook.TokenAmount memory _offer = Orderbook.TokenAmount({
+            token: address(offeredToken),
+            amount: _offeredAmount
+        });
+
+        uint256 constraints = orderbook.encodeConstraints(
+            _validFrom,
+            _validUntil,
+            _slippageBps
+        );
+
+        IERC20(address(offeredToken)).approve(
+            address(orderbook),
+            _offeredAmount
+        );
+        bytes32 offerId = orderbook.createTokenOffer(
+            _offer,
+            address(requestedToken),
+            constraints
+        );
+        vm.stopPrank();
+
+        (Orderbook.Offer memory offer, Orderbook.OfferStatus status) = orderbook
+            .getOffer(offerId);
+
+        assertEq(offer.maker, maker);
+        assertEq(offer.offer.token, address(offeredToken));
+        assertEq(offer.offer.amount, _offeredAmount);
+        assertEq(offer.constraints, constraints);
+        assertEq(offer.requestedToken, address(requestedToken));
+        assert(status == Orderbook.OfferStatus.Open);
+
+        (uint64 validFrom, uint64 validUntil, uint128 slippageBps) = orderbook
+            .decodeConstraints(offer.constraints);
+        assertEq(slippageBps, _slippageBps);
+        assertEq(validFrom, _validFrom);
+        assertEq(validUntil, _validUntil);
     }
 }
