ddd103e79f77def7e5d507814ac96fbb07659bf4
Myles-Traut <myles@linumlabs.com>
2025-12-02 10:01:33 +0200

    Orderbook: Update Constraints

--- test/Orderbook/UpdateOfferConstraints.t.sol
+++ test/Orderbook/UpdateOfferConstraints.t.sol
@@ -1,320 +1,165 @@
- // // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.25;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
 
-// import {TestSetup} from "../TestSetup.t.sol";
-// import {Orderbook} from "../../src/contracts/Orderbook.sol";
+import {TestSetup} from "../TestSetup.t.sol";
+import {Orderbook} from "../../src/contracts/Orderbook.sol";
 
-// contract UpdateOfferTest is TestSetup {
-//     uint256 public newOfferAmount = OFFER_AMOUNT / 2;
-//     uint256 public newMinFillAmount = MIN_FILL_AMOUNT / 2;
-//     uint256 public newMaxSlippageBps = MAX_SLIPPAGE_BPS / 2;
-//     uint256 public newValidFrom = validFrom + 100;
-//     uint256 public newValidUntil = validUntil + 100;
+contract UpdateOfferConstraintsTest is TestSetup {
+    uint256 public newMaxSlippageBps = MAX_SLIPPAGE_BPS / 2;
+    uint256 public newValidFrom = validFrom + 100;
+    uint256 public newValidUntil = validUntil + 100;
 
-//     address public newRequestedToken = makeAddr("newRequestedToken");
+    /*//////////////////////////////////////////////////////////////
+                            STATE UPDATES
+    //////////////////////////////////////////////////////////////*/
 
-//     /*//////////////////////////////////////////////////////////////
-//                             STATE UPDATES
-//     //////////////////////////////////////////////////////////////*/
+    function test_UpdateOfferConstraints() public {
+        bytes32 offerId = _createAndReturnOffer(
+            address(offeredToken),
+            address(requestedToken)
+        );
 
-//     function test_UpdateOffer_SameRequestedToken_Success() public {
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
+        (, , , Orderbook.Constraints memory constraints_, ) = orderbook.offers(
+            offerId
+        );
 
-//         (
-//             address maker_,
-//             Orderbook.TokenAmount memory offer_,
-//             address requestedToken_,
-//             Orderbook.Constraints memory constraints_,
-//             uint256 remainingAmount
-//         ) = orderbook.offers(offerId);
+        assertEq(constraints_.maxSlippageBps, MAX_SLIPPAGE_BPS);
+        assertEq(constraints_.validFrom, validFrom);
+        assertEq(constraints_.validUntil, validUntil);
 
-//         assertEq(maker_, maker);
-//         assertEq(offer_.token, address(offeredToken));
-//         assertEq(offer_.amount, OFFER_AMOUNT);
-//         assertEq(requestedToken_, address(requestedToken));
-//         assertEq(constraints_.minFillAmount, MIN_FILL_AMOUNT);
-//         assertEq(constraints_.maxSlippageBps, MAX_SLIPPAGE_BPS);
-//         assertEq(constraints_.validFrom, validFrom);
-//         assertEq(constraints_.validUntil, validUntil);
-//         assertEq(remainingAmount, OFFER_AMOUNT);
+        (
+            ,
+            Orderbook.Constraints memory newConstraints
+        ) = _generateOfferAmountsAndConstraints(
+                address(offeredToken),
+                OFFER_AMOUNT,
+                newMaxSlippageBps,
+                newValidFrom,
+                newValidUntil
+            );
 
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 newOfferAmount,
-//                 newMinFillAmount,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
+        vm.startPrank(maker);
 
-//         vm.startPrank(maker);
+        vm.expectEmit(true, true, true, true);
+        emit Orderbook.OfferConstraintsUpdated(offerId, maker, newConstraints);
 
-//         vm.expectEmit(true, true, true, true);
-//         emit Orderbook.OfferUpdated(
-//             offerId,
-//             maker,
-//             newOffer,
-//             address(requestedToken),
-//             newConstraints
-//         );
+        orderbook.updateConstraints(offerId, newConstraints);
+        vm.stopPrank();
 
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(requestedToken)
-//         );
-//         vm.stopPrank();
+        (
+            address _maker,
+            Orderbook.TokenAmount memory tokenAmounts,
+            address _requestedToken,
+            Orderbook.Constraints memory updatedConstraints,
+            uint256 remainingAmount
+        ) = orderbook.offers(offerId);
 
-//         (
-//             maker_,
-//             offer_,
-//             requestedToken_,
-//             constraints_,
-//             remainingAmount
-//         ) = orderbook.offers(offerId);
+        // Assert Constraints have updated. Other values remain the same.
+        assertEq(updatedConstraints.maxSlippageBps, newMaxSlippageBps);
+        assertEq(updatedConstraints.validFrom, newValidFrom);
+        assertEq(updatedConstraints.validUntil, newValidUntil);
 
-//         assertEq(offer_.token, address(offeredToken));
-//         assertEq(offer_.amount, newOfferAmount);
-//         assertEq(constraints_.minFillAmount, newMinFillAmount);
-//         assertEq(constraints_.maxSlippageBps, newMaxSlippageBps);
-//         assertEq(constraints_.validFrom, newValidFrom);
-//         assertEq(constraints_.validUntil, newValidUntil);
-//         assertEq(remainingAmount, OFFER_AMOUNT);
-//         assertEq(requestedToken_, address(requestedToken));
-//         assertEq(maker_, maker);
-//     }
+        assertEq(tokenAmounts.token, address(offeredToken));
+        assertEq(tokenAmounts.amount, OFFER_AMOUNT);
+        assertEq(remainingAmount, OFFER_AMOUNT);
+        assertEq(_requestedToken, address(requestedToken));
+        assertEq(_maker, maker);
+    }
 
-//     function test_UpdateOffer_DifferentRequestedToken_Success() public {
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
+    /*//////////////////////////////////////////////////////////////
+                                 NEGATIVE TESTS
+        //////////////////////////////////////////////////////////////*/
 
-//         (
-//             address maker_,
-//             Orderbook.TokenAmount memory offer_,
-//             address requestedToken_,
-//             Orderbook.Constraints memory constraints_,
-//             uint256 remainingAmount
-//         ) = orderbook.offers(offerId);
+    function test_UpdateOffer_Reverts_NotOfferCreator() public {
+        address invalidCaller = makeAddr("invalidCaller");
+        bytes32 offerId = _createAndReturnOffer(
+            address(offeredToken),
+            address(requestedToken)
+        );
 
-//         assertEq(maker_, maker);
-//         assertEq(offer_.token, address(offeredToken));
-//         assertEq(offer_.amount, OFFER_AMOUNT);
-//         assertEq(requestedToken_, address(requestedToken));
-//         assertEq(constraints_.minFillAmount, MIN_FILL_AMOUNT);
-//         assertEq(constraints_.maxSlippageBps, MAX_SLIPPAGE_BPS);
-//         assertEq(constraints_.validFrom, validFrom);
-//         assertEq(constraints_.validUntil, validUntil);
-//         assertEq(remainingAmount, OFFER_AMOUNT);
+        (
+            ,
+            Orderbook.Constraints memory newConstraints
+        ) = _generateOfferAmountsAndConstraints(
+                address(offeredToken),
+                OFFER_AMOUNT,
+                newMaxSlippageBps,
+                newValidFrom,
+                newValidUntil
+            );
 
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 newOfferAmount,
-//                 newMinFillAmount,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
+        vm.startPrank(invalidCaller);
 
-//         vm.startPrank(maker);
+        vm.expectRevert(Orderbook.Orderbook__NotOfferCreator.selector);
+        orderbook.updateConstraints(offerId, newConstraints);
+    }
 
-//         vm.expectEmit(true, true, true, true);
-//         emit Orderbook.OfferUpdated(
-//             offerId,
-//             maker,
-//             newOffer,
-//             address(newRequestedToken),
-//             newConstraints
-//         );
+    function test_UpdateOffer_RevertsIfStatusNotOpen() public {
+        bytes32 offerId = _createAndReturnOffer(
+            address(offeredToken),
+            address(requestedToken)
+        );
 
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(newRequestedToken)
-//         );
-//         vm.stopPrank();
+        // Simulate that the offer is in progress
+        orderbook.contribute(offerId);
 
-//         (
-//             maker_,
-//             offer_,
-//             requestedToken_,
-//             constraints_,
-//             remainingAmount
-//         ) = orderbook.offers(offerId);
+        (
+            ,
+            Orderbook.Constraints memory newConstraints
+        ) = _generateOfferAmountsAndConstraints(
+                address(offeredToken),
+                OFFER_AMOUNT,
+                newMaxSlippageBps,
+                newValidFrom,
+                newValidUntil
+            );
 
-//         assertEq(offer_.token, address(offeredToken));
-//         assertEq(offer_.amount, newOfferAmount);
-//         assertEq(constraints_.minFillAmount, newMinFillAmount);
-//         assertEq(constraints_.maxSlippageBps, newMaxSlippageBps);
-//         assertEq(constraints_.validFrom, newValidFrom);
-//         assertEq(constraints_.validUntil, newValidUntil);
-//         assertEq(remainingAmount, OFFER_AMOUNT);
-//         assertEq(requestedToken_, address(newRequestedToken));
-//         assertEq(maker_, maker);
-//     }
+        vm.startPrank(maker);
 
-//     /*//////////////////////////////////////////////////////////////
-//                              NEGATIVE TESTS
-//     //////////////////////////////////////////////////////////////*/
+        vm.expectRevert(Orderbook.Orderbook__OfferNotOpen.selector);
+        orderbook.updateConstraints(offerId, newConstraints);
+        vm.stopPrank();
+    }
 
-//     function test_UpdateOffer_Reverts_NotOfferCreator() public {
-//         address invalidCaller = makeAddr("invalidCaller");
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
+    function test_UpdateOffer_RevertsOnInvalidConstraints() public {
+        bytes32 offerId = _createAndReturnOffer(
+            address(offeredToken),
+            address(requestedToken)
+        );
 
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 newOfferAmount,
-//                 newMinFillAmount,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
+        vm.startPrank(maker);
 
-//         vm.startPrank(invalidCaller);
+        vm.expectRevert(Orderbook.Orderbook__InvalidConstraints.selector);
+        orderbook.updateConstraints(
+            offerId,
+            Orderbook.Constraints({
+                maxSlippageBps: 0,
+                validFrom: uint64(newValidFrom),
+                validUntil: uint64(newValidUntil)
+            })
+        );
 
-//         vm.expectRevert(Orderbook.Orderbook__NotOfferCreator.selector);
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(requestedToken)
-//         );
-//     }
+        vm.expectRevert(Orderbook.Orderbook__InvalidConstraints.selector);
+        orderbook.updateConstraints(
+            offerId,
+            Orderbook.Constraints({
+                maxSlippageBps: newMaxSlippageBps,
+                validFrom: 0,
+                validUntil: uint64(newValidUntil)
+            })
+        );
 
-//     function test_UpdateOffer_RevertsOnZeroAddress() public {
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
-
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 newOfferAmount,
-//                 newMinFillAmount,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
-
-//         vm.startPrank(maker);
-
-//         vm.expectRevert(Orderbook.Orderbook__ZeroAddress.selector);
-//         orderbook.updateOffer(offerId, newOffer, newConstraints, address(0));
-//         vm.stopPrank();
-//     }
-
-//     function test_UpdateOffer_RevertsIfStatusNotOpen() public {
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
-
-//         // Simulate that the offer is in progress
-//         orderbook.contribute(offerId);
-
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 newOfferAmount,
-//                 newMinFillAmount,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
-
-//         vm.startPrank(maker);
-
-//         vm.expectRevert(Orderbook.Orderbook__OfferNotOpen.selector);
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(requestedToken)
-//         );
-//         vm.stopPrank();
-//     }
-
-//     function test_UpdateOffer_RevertsOnInvalidTokenAmounts() public {
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
-
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 0,
-//                 newMinFillAmount,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
-
-//         vm.startPrank(maker);
-
-//         vm.expectRevert(Orderbook.Orderbook__InvalidTokenAmount.selector);
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(requestedToken)
-//         );
-//         vm.stopPrank();
-
-//         (newOffer, newConstraints) = _generateOfferAmountsAndConstraints(
-//             address(0),
-//             newOfferAmount,
-//             newMinFillAmount,
-//             newMaxSlippageBps,
-//             newValidFrom,
-//             newValidUntil
-//         );
-
-//         vm.startPrank(maker);
-
-//         vm.expectRevert(Orderbook.Orderbook__ZeroAddress.selector);
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(requestedToken)
-//         );
-//         vm.stopPrank();
-//     }
-
-//     function test_UpdateOffer_RevertsOnInvalidConstraints() public {
-//         bytes32 offerId = _createAndReturnOffer(address(requestedToken));
-
-//         // Invalid minFillAmount
-
-//         (
-//             Orderbook.TokenAmount memory newOffer,
-//             Orderbook.Constraints memory newConstraints
-//         ) = _generateOfferAmountsAndConstraints(
-//                 address(offeredToken),
-//                 newOfferAmount,
-//                 0,
-//                 newMaxSlippageBps,
-//                 newValidFrom,
-//                 newValidUntil
-//             );
-
-//         vm.startPrank(maker);
-
-//         vm.expectRevert(Orderbook.Orderbook__InvalidConstraints.selector);
-//         orderbook.updateOffer(
-//             offerId,
-//             newOffer,
-//             newConstraints,
-//             address(requestedToken)
-//         );
+        vm.expectRevert(Orderbook.Orderbook__InvalidConstraints.selector);
+        orderbook.updateConstraints(
+            offerId,
+            Orderbook.Constraints({
+                maxSlippageBps: newMaxSlippageBps,
+                validFrom: uint64(newValidFrom),
+                validUntil: uint64(newValidFrom)
+            })
+        );
+    }
+}
 
 //         // Invalid maxSlippageBps
 
